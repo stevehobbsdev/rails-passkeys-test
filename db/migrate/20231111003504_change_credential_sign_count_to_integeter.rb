d07ac77cfeb88a9a2ea8f755f2e3ce930de092f2
Steve Hobbs <steve.hobbs@hey.com>
2024-01-04 23:11:47 +0000

    chore: use cast in sign_count column migration

--- db/migrate/20231111003504_change_credential_sign_count_to_integeter.rb
+++ db/migrate/20231111003504_change_credential_sign_count_to_integeter.rb
@@ -1,5 +1,5 @@
 class ChangeCredentialSignCountToIntegeter < ActiveRecord::Migration[7.1]
   def change
-    change_column :credentials, :sign_count, :integer
+    change_column :credentials, :sign_count, 'numeric USING CAST(sign_count as numeric)'
   end
 end
